3141d1bf5c555968313a6101bf3e7162aa2490ec
Alex Jenkins <alexjenkins@Alexs-MacBook-Pro.local>
2022-10-18 21:52:55 -0400

    update route app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import "./index.css";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Project from "./routes/Project";
+import Contact from "./routes/Home";
+
+import { Route, Routes} from "react-router-dom"
 
 
 function App() {
-  return <div>
-    <h1> Test </h1>
-  </div>;
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Home /> } />
+      <Route path="/project" element={<Project /> } />
+      <Route path="/" element={<About /> } />
+      <Route path="/" element={<Contact /> } />
+    </Routes>
+    </>
+  );
 }
 
 export default App;
