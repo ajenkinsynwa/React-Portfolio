c8f661deab21154a5d179a12217c3c523209355b
Alex Jenkins <alexjenkins@Alexs-MacBook-Pro.local>
2022-10-19 22:55:05 -0400

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,14 +2,39 @@ import "./FooterStyles.css"
 
 import React from 'react'
 
+import {FaGithub,  FaLinkedin, FaMailBulk, FaTwitter } from "react-icons/fa";
+
 function Footer() {
   return (
     <div class="footer">
       <div className="footer-container">
         <div className="left"></div>
-        <div className="right"></div>
+        
+      
+       
+          
+        </div>
+        
+
+        <div className="right">
+          <h4> Social Media Links </h4>
+          <p> Links to social media </p>
+          <div className="social">
+
+          <FaGithub size={30} style={{ color:
+          "#fff", marginRight: "1rem"}} />
+
+          <FaTwitter size={30} style={{ color:
+          "#fff", marginRight: "1rem"}} />
+
+          <FaLinkedin size={30} style={{ color:
+          "#fff", marginRight: "1rem"}} />
+        
+
+          </div>
+        </div>
       </div>
-    </div>
+    
   )
 }
 
