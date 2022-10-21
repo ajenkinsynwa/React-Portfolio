0e51c59d50b0b5595308d883c208b540ab51b5cb
Alex Jenkins <alexjenkins@Alexs-MacBook-Pro.local>
2022-10-20 22:14:57 -0400

    updated ProjectsCard

--- src/components/ProjectsCard.js
+++ src/components/ProjectsCard.js
@@ -10,7 +10,8 @@ function ProjectsCard() {
       <h1 className="project-heading"> Projects </h1>
       <div className="project-container">
         <div className="project-card">
-          <img src={pro1} alt="img" /> 
+          <div>
+          <img className="img"  src={pro1} alt="img" /> </div>
           <h2 className="project-title">Project Title</h2>
           <div className="pro-details">
             <p> This is Text</p>
@@ -19,13 +20,42 @@ function ProjectsCard() {
               className="btn">View Project</NavLink>
               <NavLink to="https://github.com/tedtalktimmy/queen-city-pup"
               className="btn">Repository</NavLink>
-
-            </div>
+              </div>
+          </div>
+        </div>
+        <div className="project-card">
+          <div>
+          <img className="img"  src={pro1} alt="img" /> </div>
+          <h2 className="project-title">Project Title</h2>
+          <div className="pro-details">
+            <p> This is Text</p>
+            <div className="pro-btns">
+              <NavLink to="https://evening-river-11733.herokuapp.com/"
+              className="btn">View Project</NavLink>
+              <NavLink to="https://github.com/tedtalktimmy/queen-city-pup"
+              className="btn">Repository</NavLink>
+              </div>
+          </div>
+        </div>
+        <div className="project-card">
+          <div>
+          <img className="img"  src={pro1} alt="img" /> </div>
+          <h2 className="project-title">Project Title</h2>
+          <div className="pro-details">
+            <p> This is Text</p>
+            <div className="pro-btns">
+              <NavLink to="https://evening-river-11733.herokuapp.com/"
+              className="btn">View Project</NavLink>
+              <NavLink to="https://github.com/tedtalktimmy/queen-city-pup"
+              className="btn">Repository</NavLink>
+              </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+
+    
+  );
+};
 
 export default ProjectsCard
