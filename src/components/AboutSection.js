ecd5289bc8d423cf2938880d00638815e142a0c0
Alex Jenkins <alexjenkins@Alexs-MacBook-Pro.local>
2022-10-21 05:50:37 -0400

    updated About Section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,10 +1,26 @@
-import "./AboutContentStyles.css"
+import "./AboutSection.css"
 
 import React from 'react'
 
+import React1 from "../assets/profile.jpeg";
+
 function AboutSection() {
   return (
-    <div>AboutSection</div>
+    <div className="about">
+      <div className="left">
+        <h1> Hello, I am Alex Jenkins.</h1>
+        <p>Web developer currently earning a certificate in Full Stack Development from UNC Charlotte Full Stack Coding Bootcamp in November 2022. I developed skills in JavaScript, CSS, React, responsive web design, and more. I have always had a passion for computers, technology, and web development. My previous hospitality management experience I gained communication, collaborative spirit , activate-listening, and problem solving skills that are essential to a web developer. As an innovative problem-solver passionate about developing apps, my current focus is on mobile-first design and development. I am always continuing my studies independently to learn more about iOS and Android development.</p>
+     
+      </div>
+      <div className="right">
+        <div className="img-container">
+
+        </div>
+        <div className="img-stack-top"></div>
+        <img src={React1} className="img2" 
+        alt="profileimage"/>
+      </div>
+    </div>
   )
 }
 
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -8,6 +8,7 @@ const Contact = () => {
    <div>
     <Navbar />
     <HeroImg2 heading="Contact." text="Contact Me" />
+    
     <Footer />
    </div>
   )
