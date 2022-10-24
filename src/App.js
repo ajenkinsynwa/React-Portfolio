3610538275745c24c13a0582851f772bea657987
Alex Jenkins <alexjenkins@Alexs-MacBook-Pro.local>
2022-10-24 16:07:11 -0400

    updated packagejson

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ import { Route, Routes} from "react-router-dom"
 
 function App() {
   return (
-    <>
+    
     <Routes>
       <Route path="/" element={<Home /> } />
       <Route path="/project" element={<Project /> } />
       <Route path="/about" element={<About /> } />
       <Route path="/contact" element={<Contact /> } />
     </Routes>
-    </>
+    
   );
 }
 
