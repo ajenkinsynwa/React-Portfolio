b517bef2f89bb4c9317f1e2e075920d60ccd44fb
Alex Jenkins <108849441+ajenkinsynwa@users.noreply.github.com>
2022-12-07 11:02:46 -0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Home /> } />
+      <Route path="/react-portfolio" element={<Home /> } />
       <Route path="/project" element={<Project /> } />
       <Route path="/about" element={<About /> } />
       <Route path="/contact" element={<Contact /> } />
